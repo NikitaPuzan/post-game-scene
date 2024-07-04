712a4c2408509ac1a3a12f67f9141a657b22ef3e
nikita <1phoenixnik1@gmail.com>
2024-07-04 09:08:15 +0300

    change Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import './css/header.css'
+import HeaderItems from "./HeaderItems";
 
 const Header = () => {
-    const headerComponent =  <div className="header">
-        <div className="header-item">Nickname</div>
-        <div className="header-item">Score</div>
-        <div className="header-item">Status</div>
-    </div>;
-
     return (
         <div className="header-container">
-            {headerComponent}
-            {headerComponent}
+            <HeaderItems/>
+            <HeaderItems/>
         </div>
     );
 };
---
+++ src/components/Header/HeaderItems.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderItems = () => {
+    return (
+        <div className="header">
+            <div className="header-item">Nickname</div>
+            <div className="header-item">Score</div>
+            <div className="header-item">Status</div>
+        </div>
+    );
+};
+
+export default HeaderItems;
